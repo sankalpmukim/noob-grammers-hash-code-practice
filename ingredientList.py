544b73f3c213c18f5451c7bd244f317e3cbb1c7d
sankalpmukim <sankalpmukim@gmail.com>
2021-01-13 23:44:05 +0530

    ingredientList(File) returns list of unique ingredients in sorted manner. This can be used to write the file.

---
+++ ingredientList.py
@@ -0,0 +1,13 @@
+def ingredientList(File):
+    File.__next__()
+    setOfIngredients = set()
+    lines = File.readlines()
+    for line in lines:
+        temp = line.split()
+        for i in temp[1:]:
+            setOfIngredients.add(i)
+    return sorted(list(setOfIngredients))
+
+if(__name__ == "__main__"):
+    with open('a_example') as File:
+        print(ingredientList(File))
