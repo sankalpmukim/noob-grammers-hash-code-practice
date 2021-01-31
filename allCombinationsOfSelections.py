531e70ad5b4c15f27450c78e0c8f035006776c55
sankalpmukim <sankalpmukim@gmail.com>
2021-02-01 01:15:59 +0530

    basic fixing bugs and maintenance

--- allCombinationsOfSelections.py
+++ allCombinationsOfSelections.py
@@ -6,21 +6,21 @@
 # That is the ultimate bruteforce
 import itertools
 from CSVConverter import CSVConverter
-def allCombinationsOfSelections():
-    with open('a_example',newline='') as File:
-        # set data.csv file
-        CSVConverter(File)
-        File.seek(0,0)
-        allLines = list(File.readlines())
-        totalTeams = sum([int(i) for i in allLines[0].split()[1:]])
-        output = []
-        for i in range(1,totalTeams+1):
-            allSelectionsOfTeams = [i for i in itertools.combinations(list('2'*int(allLines[0].split()[1:][0])+'3'*int(allLines[0].split()[1:][1])+'4'*int(allLines[0].split()[1:][2])),i)]
-            allSelectionsOfTeams = set(allSelectionsOfTeams)
-            output.append(allSelectionsOfTeams)
+def allCombinationsOfSelections(File):
+    # set data.csv file
+    CSVConverter(File)
+    File.seek(0,0)
+    allLines = list(File.readlines())
+    totalTeams = sum([int(i) for i in allLines[0].split()[1:]])
+    output = []
+    for i in range(1,totalTeams+1):
+        allSelectionsOfTeams = [i for i in itertools.combinations(list('2'*int(allLines[0].split()[1:][0])+'3'*int(allLines[0].split()[1:][1])+'4'*int(allLines[0].split()[1:][2])),i)]
+        allSelectionsOfTeams = set(allSelectionsOfTeams)
+        output.append(allSelectionsOfTeams)
     return output
 
 if __name__ == "__main__":
-    output = allCombinationsOfSelections()
-    for i in output:
-        print(i)
+    with open('a_example',newline='') as File:
+        output = allCombinationsOfSelections(File)
+        for i in output:
+            print(i)
