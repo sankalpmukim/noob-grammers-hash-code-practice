9a74325e879cafb4923d41ae94d96b3749ea75a3
sankalpmukim <sankalpmukim@gmail.com>
2021-02-07 18:08:31 +0530

    ✔😍improving efficiency using generators

--- allCombinationsOfSelections.py
+++ allCombinationsOfSelections.py
@@ -10,17 +10,17 @@ def allCombinationsOfSelections(File):
     # set data.csv file
     CSVConverter(File)
     File.seek(0,0)
-    allLines = list(File.readlines())
+    allLines = File.readlines()
     totalTeams = sum([int(i) for i in allLines[0].split()[1:]])
-    output = []
     for i in range(1,totalTeams+1):
-        allSelectionsOfTeams = [i for i in itertools.combinations(list('2'*int(allLines[0].split()[1:][0])+'3'*int(allLines[0].split()[1:][1])+'4'*int(allLines[0].split()[1:][2])),i)]
-        allSelectionsOfTeams = set(allSelectionsOfTeams)
-        output.append(allSelectionsOfTeams)
-    return output
+        allSelectionsOfTeams = (itertools.combinations(list('2'*int(allLines[0].split()[1:][0])+'3'*int(allLines[0].split()[1:][1])+'4'*int(allLines[0].split()[1:][2])),i))
+        yield(allSelectionsOfTeams)
 
 if __name__ == "__main__":
-    with open('a_example',newline='') as File:
+    with open('a_example',newline='') as File:#b_little_bit_of_everything.in
         output = allCombinationsOfSelections(File)
         for i in output:
-            print(i)
+            print('*new-*')
+            for j in set(i):
+                print(j)
+            
